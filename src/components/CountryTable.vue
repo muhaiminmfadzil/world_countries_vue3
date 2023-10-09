354b710150264614284ecb14232be67974ebc402
muhaimin <meenkun4649@gmail.com>
2023-10-09 20:49:58 +0800

    minor

--- src/components/CountryTable.vue
+++ src/components/CountryTable.vue
@@ -27,9 +27,8 @@ const countries = [
           <MagnifyingGlassIcon class="w-4 h-4 text-gray-400" aria-hidden="true" />
         </div>
         <input
-          type="email"
-          name="email"
-          id="email"
+          type="text"
+          name="search"
           class="block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           placeholder="Search by country or capital name"
         />
