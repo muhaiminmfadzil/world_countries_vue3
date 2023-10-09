0218e24efe6db91c059cf94c084699276a20b503
muhaimin <meenkun4649@gmail.com>
2023-10-09 20:44:08 +0800

    country table layout init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from '../components/HelloWorld.vue'
+import CountryTable from '@/components/CountryTable.vue'
 </script>
 
 <template>
-  <main>
-    <HelloWorld />
+  <main class="max-w-[75dvw] mx-auto p-5">
+    <!-- Heading -->
+    <h1 class="text-3xl font-semibold">World Countries</h1>
+    <!-- Descriptions -->
+    <p class="mt-2 text-sm text-gray-700">A list of all the countries in the world</p>
+    <CountryTable />
   </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindForm from '@tailwindcss/forms'
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: [tailwindForm]
 }
