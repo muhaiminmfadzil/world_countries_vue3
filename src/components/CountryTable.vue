759e63cc253e29155f26502106255bc6f83d8751
muhaimin <meenkun4649@gmail.com>
2023-10-11 23:52:28 +0800

    cleanup

--- src/components/CountryTable.vue
+++ src/components/CountryTable.vue
@@ -5,7 +5,6 @@ import CountryTableBody from '@/components/CountryTableBody.vue'
 
 <template>
   <div class="px-4 sm:px-6 lg:px-8">
-    <!-- <div class="flow-root"> -->
     <div class="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
       <div class="w-full py-2 overflow-auto">
         <table class="w-full border-separate border-spacing-0">
@@ -14,7 +13,6 @@ import CountryTableBody from '@/components/CountryTableBody.vue'
           <!-- Table Body -->
           <CountryTableBody data-test="table-body" />
         </table>
-        <!-- </div> -->
       </div>
     </div>
   </div>
